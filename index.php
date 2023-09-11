ca1431c3f0a551f86b9c82ec478ecba86cd63b32
Mathieu Gerard <mathieu2991@gmail.com>
2023-09-11 11:29:55 +0200

    ajout du footer

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         $manager->displayDestination($manager->getAllDestination());
     ?>
 </div>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+    include('footer.php');
+?>
