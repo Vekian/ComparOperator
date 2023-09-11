e25ef5bec1fb0676eb2f0d0f2dbfd1036f790aba
Mathieu Gerard <mathieu2991@gmail.com>
2023-09-11 20:03:43 +0200

    ajout de partie avis

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <header>
         <nav class="navbar navbar-light bg-light">
             <div class="container">
-                <a href="index.php"><img src="images/logo.gif" height="80"/></a>
+                <a href="index.php"><img src="images/logo.png" height="100"/></a>
                 <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
