5d7f42eb615298850092e8a6103944477c0b39d3
Mathieu Gerard <mathieu2991@gmail.com>
2023-09-14 16:56:50 +0200

    ajout css avis

--- destination.php
+++ destination.php
@@ -1,7 +1,7 @@
 <?php
     include('header.php');
 ?>
-<div class="text-center">
+<div>
 
 <?php
     $manager = new Manager($db);
@@ -9,15 +9,18 @@
         $name = $_GET['name'];
         $operators = $manager->getOperatorByDestination($name);
         $picture = $manager->getDestinationByOperator($operators[0], $name)->getPicture();
-        echo('<img src="'. $picture .'" height="300px"/>');
+        echo('<img src="'. $picture .'" height="300px" id="destinationMainImg" class="mb-3"/>');
         foreach($operators as $operator){
             $destination = $manager->getDestinationByOperator($operator, $name);
             $manager->displayTourOperator($operator, $destination);
         }
     }
 ?>
-
-
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
 </div>
 <?php
     include('footer.php');
