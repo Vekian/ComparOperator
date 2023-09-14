21628b53c98a514adb240206a8c1980c09c29826
YvanNours~ <yvan.bardel@gmail.com>
2023-09-14 12:24:17 +0200

    log admin

--- footer.php
+++ footer.php
@@ -16,12 +16,12 @@
             <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
             <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
         </div>
-        <!-- <div class="container-paiement">
+        <div class="container-paiement">
                         <p>Paiement sécurisé</p>
             <i class="fa-brands fa-cc-mastercard" style="color: #ffffff;"></i>
             <i class="fa-brands fa-cc-visa" style="color: #ffffff;"></i>
             <i class="fa-brands fa-paypal" style="color: #ffffff;"></i>
-        </div> -->
+        </div>
      </nav>
 </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
